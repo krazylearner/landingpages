38f83eb4698e4fcd30163f4f96b257291a21a9cc
ankur <krazylearner@gmail.com>
2016-02-19 20:32:46 +0530

    moved chat pages to approved folder

---
+++ landing pages/testmode landing pages/quickbooks.tk (MS))/disclaimer.php
@@ -0,0 +1,171 @@
+<?php include "recorder.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+	<title>Disclaimer</title>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	
+
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	
+	<script type="text/javascript">
+// Popup window code
+function newPopup(url) {
+  popupWindow = window.open(
+    url,'popUpWindow','height=500,width=500,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}
+</script>
+</head>
+<body>
+<a href="https://tawk.to/f767cf69a69175bdc6d312cb1cc79c8f4b9cb510/popout/default/?$_tawk_popout=true&$_tawk_sk=56b3c85ef60b7a92846a7c2e&$_tawk_tk=636ee4a00e41dffdb8b590a2d00f5e37&v=458" target="_blank" onclick="goog_report_conversion ('https://tawk.to/f767cf69a69175bdc6d312cb1cc79c8f4b9cb510/popout/default/?$_tawk_popout=true&$_tawk_sk=56b3c85ef60b7a92846a7c2e&$_tawk_tk=636ee4a00e41dffdb8b590a2d00f5e37&v=458')"><img id="feedback" src ="css/images/live_chat2.png" /></a>
+	<!-- wrapper -->
+	<div class="wrapper">
+		<!-- header -->
+		<header class="header">
+			<div class="shell">
+				<div class="header-top">
+					<h1 id="logo"><a href="#">Digy</a></h1>
+					<nav id="navigation">
+						<a href="#" class="nav-btn">Home<span></span></a>
+						<ul>
+							<li class="active home"><a href="index.php">Home</a></li>
+							<li><a href="privacy-policy.php">Privacy Policy</a></li>
+							<li><a href="plans.php">Plans</a></li>
+							<li><a href="refund-policy.php">Refund Policy </a></li>
+							<li><a href="disclaimer.php">Disclaimer</a></li>
+							<li><a href="contact.php">Contact Us</a></li>
+						</ul>
+					</nav>
+					<div class="cl">&nbsp;</div>
+				</div>
+				
+			</div>
+		</header>
+		<!-- end of header -->
+		<!-- shell -->
+		<div class="shell">
+			<!-- main -->
+			<div class="main">
+				<!-- cols -->
+				
+				
+
+				<section class="content">
+					<h2>Disclaimer</h2>
+					<p>
+			<b>1. INDEPENDENT SERVICE PROVIDER:</b> avgtech.tk is an independent third party service provider for several brands. The service we offer is also available on the website of the brand owner. The services can be offered even if the product is on warranty or not. Use of names, trademarks is for reference only. We disclaim any ownership, right, affiliation or endorsement of or by any such third party products or brands.<br>
+
+<b>2. CERTIFIED TECHNICIANS:</b> avgtech's support staff are avgtech Certified Technicians but do not necessarily hold any certifications from any third party unless expressly specified.<br>
+
+<b>3. COPYRIGHTS AND TRADEMARKS:</b> The trademarks avgtech, the avgtech character and any combination of these marks are the trademarks of avgtech.com and/or its subsidiaries and affiliates. In addition, the look and feel of the Site (including all page headers, custom graphics, button icons, and scripts) is the service mark and/or trademark of avgtech and may not be copied, imitated, or used (in whole or in part) without avgtech's prior written permission.<br>
+
+4. All other trademarks, registered trademarks, company names, product names and brand names are the property of their respective owners, and avgtech disclaims any ownership in such third-party marks. The use of any third party trademarks, logos, or brand names is for informational purposes only, and does not imply an endorsement by avgtech or vice versa or that such trademark owner has authorized avgtech to promote its products or services.<br>
+
+<b>5. WARRANTY COVERAGE:</b> Most hardware and software products are covered under manufacturer’s warranty during the initial period of use. If your product is under warranty the support services may be available for free. avgtech recommends that you contact the original supplier of the product in such circumstances.<br>
+
+<b>6. CONTENT ON avgtech WEBSITE:</b> The avgtech Portal, and all content provided on or through the site, are provided on an "as is" and "as available" basis, except where expressly provided otherwise. avgtech is not responsible for third party Content provided on or through the Site and you bear all risks associated with the use of such third party Content, products and services.<br>
+
+<b>7. CUSTOMER TESTIMONIALS:</b> Testimonials found on this site and other related sites are unverified results that have been emailed by our customers or posted on social media networks. They may not reflect the typical purchaser's experience, may not apply to the average person and are not intended to represent or guarantee that anyone will experience the same or similar results.<br>
+
+<b>8. REMOTE ACCESS TO COMPUTER:</b> We recommend that you review the process of remote access. <br>
+
+<b>9. SERVICE EXCLUSION:</b> Please refer to Section 3.3 "Exclusion from Services" in the avgtech service terms and conditions to ensure that you fully understand services that are not included in the 1-year or multi-year maintenance contract.<br>
+
+<b>10. THIRD-PARTY DIAGNOSTIC SOFTWARE:</b> avgtech when necessary use third-party software for diagnosing and fixing the problem. The software tools used are from leading companies such as Microsoft (e.g. Microsoft Security Essentials) or AVG (AVG Tune up) etc. Some of these third-party tools are available for free that you can obtain directly from these sites. avgtech does not charge for this software.<br>
+
+<b>11. PROPRIETARY SOFTWARE:</b> avgtech currently does not use or sell any proprietary computer repair software or diagnostic tools.<br>
+
+<b>12. CUSTOMER SATISFACTION:</b> The customer satisfaction metric is based on an online survey that all of our users are given the option of completing at the end of each repair session. Not all users complete this online survey. This metric is not based on a third-party audit by a market research firm specializing in customer satisfaction tracking studies. As such this metric may not be statistically accurate measurement of avgtech's customer satisfaction.<br>
+
+<b>13. NO AUTOMATIC RENEWALS:</b> Currently it is our policy to not automatically renew our subscribers.<br>
+
+<b>14. avgtech COOKIE:</b> To enhance our user experience we have implemented cookies on our website.
+</p>
+
+					
+				</section>
+
+				
+
+				
+			</div>
+			<!-- end of main -->
+		</div>
+		<div id="footer">
+			<!-- shell -->
+			<div class="shell">
+				<!-- footer-cols -->
+				<div class="footer-cols">
+					<div class="col">
+						<h2>Support For AVG </h2>
+						<ul>
+							<li><a href="#">avg technical help</a></li>
+							<li><a href="#">avg install and set up</a></li>
+							<li><a href="#">avg renewal problems</a></li>
+							<li><a href="#">avg trojan support</a></li>
+							<li><a href="#">avg virus defination updates</a></li>
+							<li><a href="#">avg malware removal</a></li>
+						</ul>
+					</div>
+
+					
+
+					<div class="col">
+						<h2>BLOG</h2>
+						<ul>
+							<li><a href="https://support.avg.com/SupportArticleView?l=en_US&urlName=What-to-do-when-AVG-installation-is-failing"> AVG installation is failing</a></li>
+							<li><a href="https://support.avg.com/SupportArticleView?l=en_US&urlName=General-Error-when-updating-AVG">General Error when updating AVG </a></li>
+							<li><a href="https://support.avg.com/answers?id=906b00000008nCTAAY">Installing AVG Windows ZEN Error code: 0xC0070643 </a></li>
+							<li><a href="https://support.avg.com/SupportArticleView?l=en_US&urlname=Message-Installing-3rd-party-redistributables-failed-when-installing-AVG">Installing 3rd party redistributables failed</a></li>
+							<li><a href="https://support.avg.com/search?l=en_US&retUrl=SupportArticleView&searchText=Message+Verify+that+you+have+sufficient+privileges+when+installing+AV&supportType=home">Insufficient privilages when installing AVG</a></li>
+						</ul>
+					</div>
+
+					<div class="col">
+						<h2>CONTACT us</h2>
+
+						<p>Email: <a href="#">support@avgtech.tk</a></p>
+						<p>Phone: 1-480-616-7013</p>
+						<p>Address: 5643 W Bonanza Ln
+</p>
+						<p>Phoenix AZ </p>
+					</div>
+					<div class="cl">&nbsp;</div>
+				</div>
+				<!-- end of footer-cols -->
+				<div class="footer-bottom">
+					<div class="footer-nav">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="privacy-policy.php">Privacy Policy</a></li>
+							<li><a href="refund-policy.php">Refund Policy </a></li>
+							<li><a href="disclaimer.php">Disclaimer</a></li>
+							<li><a href="contact.php">Contact Us</a></li>
+							<li><a href="terms.php">Terms And Conditions</a></li>
+						</ul>
+					</div>
+					<p class="copy">Copyright &copy; 2012<span>|</span>Design by: <a href="http://chocotemplates.com" target="_blank">www.ChocoTemplates.com</a></p>
+					<div class="cl">&nbsp;</div>
+				</div>
+			</div>
+			<!-- end of shell -->
+		</div>
+		<!-- footer -->
+	</div>
+	<!-- end of wrapper -->
+	<!-- Google Code for chat engaged Conversion Page
+In your html page, add the snippet and call
+goog_report_conversion when someone clicks on the
+chosen link or button. -->
+
+
+<script>
+(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5128796"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
+<noscript><img src="//bat.bing.com/action/0?ti=5128796&Ver=2" height="0" width="0" style="display:none; visibility: hidden;" /></noscript>
+ </body>
+</html>
---
+++ landing pages/testmode landing pages/quickbooks.tk (MS))/privacy-policy.php
@@ -0,0 +1,180 @@
+<?php include "recorder.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+	<title>Privacy policy</title>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	
+
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	
+	<script type="text/javascript">
+// Popup window code
+function newPopup(url) {
+  popupWindow = window.open(
+    url,'popUpWindow','height=500,width=500,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}
+</script>
+</head>
+<body>
+<a href="https://tawk.to/f767cf69a69175bdc6d312cb1cc79c8f4b9cb510/popout/default/?$_tawk_popout=true&$_tawk_sk=56b3c85ef60b7a92846a7c2e&$_tawk_tk=636ee4a00e41dffdb8b590a2d00f5e37&v=458" target="_blank" onclick="goog_report_conversion ('https://tawk.to/f767cf69a69175bdc6d312cb1cc79c8f4b9cb510/popout/default/?$_tawk_popout=true&$_tawk_sk=56b3c85ef60b7a92846a7c2e&$_tawk_tk=636ee4a00e41dffdb8b590a2d00f5e37&v=458')"><img id="feedback" src ="css/images/live_chat2.png" /></a>
+	<!-- wrapper -->
+	<div class="wrapper">
+		<!-- header -->
+		<header class="header">
+			<div class="shell">
+				<div class="header-top">
+					<h1 id="logo"><a href="#">Digy</a></h1>
+					<nav id="navigation">
+						<a href="#" class="nav-btn">Home<span></span></a>
+						<ul>
+							<li class="active home"><a href="index.php">Home</a></li>
+							<li><a href="privacy-policy.php">Privacy Policy</a></li>
+							<li><a href="plans.php">Plans</a></li>
+							<li><a href="refund-policy.php">Refund Policy </a></li>
+							<li><a href="disclaimer.php">Disclaimer</a></li>
+							<li><a href="contact.php">Contact Us</a></li>
+						</ul>
+					</nav>
+					<div class="cl">&nbsp;</div>
+				</div>
+				
+			</div>
+		</header>
+		<!-- end of header -->
+		<!-- shell -->
+		<div class="shell">
+			<!-- main -->
+			<div class="main">
+				<!-- cols -->
+				
+				
+
+				<section class="content">
+					<h2>Privacy POlicy</h2>
+					<p>
+			This privacy policy discloses the practices for avgtech.tk to protect privacy for users of its Service.
+</p>
+<p>From herein, the "Service" is defined as the user support services owned and operated by avgtech. Submitting your information on our website constitutes acceptance of this privacy Policy.</p>
+<p><b>1) INFORMATION COLLECTION AND USE:</b> avgtech is the sole owner of the information collected through the web site/phone/e-mail. We will not sell or rent this information to others in ways different from what is disclosed in this policy. avgtech collects information from the users at several different points through our toolbar.</p>
+<p><b>a) Personal Information:</b> We will ask you when we need information that personally identifies you (personal information) or allows us to contact you. In order to use the avgtech Service user must first complete the registration form. During registration user is required to give user contact information (such as First and Last name, E-mail address, Postal address, Phone number(s), Personal identifier etc.). avgtech will use your contact details to contact you regarding the product and/or services you have requested.</p>
+<p>The user (You) hereby give consent to receive autodialed and/or pre-recorded telemarketing calls (This includes re-registration/renewal notifications, special offers, and surveys on improving our service) from or on behalf of avgtech at the telephone number provided, including their wireless number, if applicable. The user (You) understands that consent is not a condition of purchase.
+</p>
+<p><b>b) Payment Information:</b> If user registers to use the Service provided by avgtech, we will collect credit card information, and other payment information necessary to complete a payment from user. avgtech may use a third party to process and verify credit cards for billing purposes. This requires us to share user name, credit card number, expiration date and billing address with the credit card processing company. When processing a payment transaction, avgtech will also obtain information regarding the amount of the payment and other transaction data. We may transfer or disclose this payment information to a third party only to the extent necessary in order to complete the payment processing.</p>
+<p><b>c) Computer System Information:</b></p>
+<p><b>i) Subscriber Computer:</b> We may also ask for information related to your computer. This may include: information about the date of purchase of your computer, type of computer, identification number of your computer, make and model of your computer and/or any computer hardware, software or peripherals attached to it, condition of the computer, system and registry data about software installations and hardware configurations, and error tracking files. Generally, this information is required to provide personalized technical support to you, and to help us update our support tools and enhance our supported products list.</p>
+<p><b>ii) Remote Access:</b> While requesting support you may request for the problem to be resolved by taking control of your computer. avgtech uses qualified tools which allow user to grant control of user computer to an Expert remotely over the Internet, so that the Expert can diagnose or correct problems. Experts are not allowed to use the Remote Access software until and unless they have received adequate training in its use, and user have consented to grant control. In addition, Experts will not use Remote Access tools to obtain confidential or sensitive information stored on user computer or network, deliberately destroy information on user computer or network, or cause user to experience system problems.
+</p>
+<p><b>iii) Diagnostic Tool:</b> Some avgtech online diagnoses application may collect a wide variety of useful information about the state of a computer system and the applications on it. This information is packaged into a document containing all the essential details needed by avgtech and is sent securely over the Internet to the avgtech server or a third party server appointed by avgtech. avgtech analyzes this information to help diagnose and solve end-user problems. Information collected by the avgtech application will not contain any sensitive information such as Web sites visited, e-mail messages, e-mail addresses sent to, passwords, profiles, etc.</p>
+<p>Use of the Remote Access and Diagnoses applications is also subject to the terms of use in the respective software license agreements.</p>
+<p><b>d) Live Help Session Records:</b> avgtech reserves the right to monitor on-line and off-line sessions between user and a Technical Support Executive for quality control of the Service provided to the Customer by Technical Support Executive. In addition, we may record on-line and off-line sessions conducted on the Web site for user reference and to assist in resolution of disputes and/or potential complaints. The session records will also be used to improve the Service, build a support knowledge base, and/or conduct internal market research. Session record data will not be correlated with personally identifiable data about individual customers.
+</p>
+<p><b>e) Cookies:</b> A cookie is a message given to a Web browser by a Web server. The browser stores the message in a text file. The main purpose of cookies is to identify users and possibly prepare customized Web pages for them. Usage of a cookie is in no way linked to any personally identifiable information while on our site and cannot infringe the privacy of our customers.</p>
+<p><b>f) Log Files:</b> We use IP addresses to analyze trends, administer the site, track user movement, and gather broad demographic information for aggregate use.
+</p>
+<p><b>g) Sharing:</b> We will share aggregated demographic information with our partners and advertisers. This is usually not linked to any personal information that can identify any individual person, unless specifically required to allow use of specific widgets within the toolbar.
+</p>
+<p>h) Surveys & Contests From time-to-time our toolbar requests information from user via surveys or contests. Participation in these surveys or contests is completely voluntary and user therefore has a choice whether or not to disclose this information. Information requested may include contact information (such as name and shipping address), and demographic information (such as zip code). Contact information will be used to notify the winners and award prizes. Survey information will be used for purposes of monitoring or improving the use and satisfaction of this toolbar and the avgtech service.</p>
+<p><b>i) Children's Online Policy:</b> No part of our website is structured to attract anyone under the age of 13. Consistent with the Children's Online privacy Protection Act, we do not collect or maintain information at our website from or sell products to those we actually know are under the age of 13. Please ask your parent or guardian to assist you in using our services if you are under the age of 13.
+</p>
+<p><b>2) SECURITY:</b> This Web site takes every precaution to protect information of users. When user submits sensitive information via the Web site, user information is protected both on-line and off-line. When our registration form asks User to enter sensitive information (such as credit card number), the information is encrypted and is protected with the best of the encryption software. While on a secure page, such as our registration form, the lock icon on the bottom of Web browsers such as Microsoft Internet Explorer becomes locked, as opposed to un-locked, or open, when User is just 'surfing'. While we use encryption to protect sensitive information online, we also do everything in our power to protect user-information off-line. All of our users' information, not just the sensitive information mentioned above, is restricted in our offices. Only employees who need the information to perform a specific job (for example, our billing clerk or a customer service representative) are granted access to personally identifiable information. Our employees use password-protected screen-savers when they leave their desk. When they return, they must re-enter their password to re-gain access to user information. Furthermore, ALL employees are kept up-to-date on our security and privacy practices. Every quarter, as well as any time new policies are added, our employees are notified and/or reminded about the importance we place on privacy, and what they can do to ensure our customers' information is protected. Finally, the servers that we store personally identifiable information on are kept in a secure environment. In situations that are required by law, we may disclose some information about our users to law-enforcing officers.</p>
+<p><b>3) SUPPLEMENTATION OF INFORMATION:</b> In order for this toolbar to properly fulfill its obligation to our customers, it is necessary for us to supplement the information we receive with information from Third party sources. For example, to determine our user's creditworthiness, we may use their name and other related information to request a credit report. Once we determine user creditworthiness, this document is destroyed.</p>
+<p><b>a) Special Offers:</b> We send all new members a welcoming e-mail to verify password and logon name. Established members will occasionally receive information on products, services, special deals, and newsletters.</p>
+
+<p><b>b) Correction/Updating Personal Information:</b> If User personally identifiable information changes (such as user zip code), or if User no longer desire the Service, we will Endeavour to provide a way to correct, update or remove User personal data provided to us. This can be done at the Profile page</p>
+<p><b>c) Notification of Changes:</b> avgtech reserves the right to update this privacy Policy at any time by posting changes online without notice to you. The most current version of the privacy Policy can be reviewed by clicking on the "Privacy Policy" hypertext link located on our web site http://www.avgtech.tk. It is your responsibility to refer to this privacy Policy before submitting any information on the website. Submitting your information after such changes to the privacy Policy constitutes acceptance of those posted changes</p>
+<p><b>d) Opt-out Provision:</b> Customers may prevent their information from being used for purposes other than those for which it was originally collected by emailing us at support@avgtech.tk</p>
+<p>e) If at any point we decide to use personally identifiable information in a manner different from that stated at the time it was collected, we will notify user via e-mail. We will use information in accordance with the privacy policy under which the information was collected.
+</p>
+<p>If user has any questions about the privacy Policy at our Web site, user can e-mail user inquiries to support@avgtech.tk</p>
+
+
+					
+				</section>
+
+				
+
+				
+			</div>
+			<!-- end of main -->
+		</div>
+		<!-- end of shell -->	
+		<!-- footer -->
+		<div id="footer">
+			<!-- shell -->
+			<div class="shell">
+				<!-- footer-cols -->
+				<div class="footer-cols">
+					<div class="col">
+						<h2>Support For AVG </h2>
+						<ul>
+							<li><a href="#">avg technical help</a></li>
+							<li><a href="#">avg install and set up</a></li>
+							<li><a href="#">avg renewal problems</a></li>
+							<li><a href="#">avg trojan support</a></li>
+							<li><a href="#">avg virus defination updates</a></li>
+							<li><a href="#">avg malware removal</a></li>
+						</ul>
+					</div>
+
+					
+
+					<div class="col">
+						<h2>BLOG</h2>
+						<ul>
+							<li><a href="https://support.avg.com/SupportArticleView?l=en_US&urlName=What-to-do-when-AVG-installation-is-failing"> AVG installation is failing</a></li>
+							<li><a href="https://support.avg.com/SupportArticleView?l=en_US&urlName=General-Error-when-updating-AVG">General Error when updating AVG </a></li>
+							<li><a href="https://support.avg.com/answers?id=906b00000008nCTAAY">Installing AVG Windows ZEN Error code: 0xC0070643 </a></li>
+							<li><a href="https://support.avg.com/SupportArticleView?l=en_US&urlname=Message-Installing-3rd-party-redistributables-failed-when-installing-AVG">Installing 3rd party redistributables failed</a></li>
+							<li><a href="https://support.avg.com/search?l=en_US&retUrl=SupportArticleView&searchText=Message+Verify+that+you+have+sufficient+privileges+when+installing+AV&supportType=home">Insufficient privilages when installing AVG</a></li>
+						</ul>
+					</div>
+
+					<div class="col">
+						<h2>CONTACT us</h2>
+
+						<p>Email: <a href="#">support@avgtech.tk</a></p>
+						<p>Phone: 1-480-616-7013</p>
+						<p>Address: 5643 W Bonanza Ln
+</p>
+						<p>Phoenix AZ </p>
+					</div>
+					<div class="cl">&nbsp;</div>
+				</div>
+				<!-- end of footer-cols -->
+				<div class="footer-bottom">
+					<div class="footer-nav">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="privacy-policy.php">Privacy Policy</a></li>
+							<li><a href="refund-policy.php">Refund Policy </a></li>
+							<li><a href="disclaimer.php">Disclaimer</a></li>
+							<li><a href="contact.php">Contact Us</a></li>
+							<li><a href="terms.php">Terms And Conditions</a></li>
+						</ul>
+					</div>
+					<p class="copy">Copyright &copy; 2012<span>|</span>Design by: <a href="http://chocotemplates.com" target="_blank">www.ChocoTemplates.com</a></p>
+					<div class="cl">&nbsp;</div>
+				</div>
+			</div>
+			<!-- end of shell -->
+		</div>
+		<!-- footer -->
+	</div>
+	<!-- end of wrapper -->
+	<!-- Google Code for chat engaged Conversion Page
+In your html page, add the snippet and call
+goog_report_conversion when someone clicks on the
+chosen link or button. -->
+
+
+<script>
+(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5128796"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
+<noscript><img src="//bat.bing.com/action/0?ti=5128796&Ver=2" height="0" width="0" style="display:none; visibility: hidden;" /></noscript>
+ </body>
+</html>
